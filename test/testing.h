34cea7f12a61ee44631631125d0b461ed480f6d9
rasputin <martapopa1978@gmail.com>
2017-05-20 00:10:17 +0700

    Add void

--- test/testing.h
+++ test/testing.h
@@ -6,6 +6,21 @@
 #include <QTest>
 #include "../mainwindow.h"
 
+class Testing : public QObject
+{
+	Q_OBJECT
+private:
+	MainWindow *wnd;
 
+private slots:
+	void create_obj();
+	void ParsingTestFull();
+	void ParsingTestEmpty();
+	void HashMap_parse_correct_full();
+	void HashMap_parse_correct_empty();
+	void TableWidget_insertion_correct_full();
+	void TableWidget_insertion_correct_empty();
+	void clean();
+};
 
 #endif // TESTING_H

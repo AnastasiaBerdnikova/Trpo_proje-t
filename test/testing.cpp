73c0f24a2db96cb5c21982c1cefe4e73fa90eea3
rasputin <martapopa1978@gmail.com>
2017-05-19 23:32:56 +0700

    Add сhecking the operation of the function of dividing the text into words

--- test/testing.cpp
+++ test/testing.cpp
@@ -6,3 +6,30 @@
 #include <QFile>
 #include <QByteArray>
 #include "../mainwindow.h"
+
+QTEST_MAIN(Testing)
+
+void Testing::create_obj()
+{
+	wnd = new MainWindow(0);
+}
+
+void Testing::ParsingTestFull() // тест для проверки работы функции разбиения текста на слова
+{
+	QString filename = QApplication::applicationDirPath() + "/input.txt";
+
+	QStringList list = wnd->GetListOfWords(filename);
+	QStringList list2;
+	QString buff;
+	QFile f("input.txt");
+	f.open(QFile::ReadOnly);
+
+	buff = f.readAll();
+	buff.remove(QRegExp("[\\n\\t\\r!@#$%^&*()-=_+|;':\",.<>?']"));
+
+	list2 = buff.split(QRegExp("\\s"));
+
+
+	QCOMPARE(list.size(), list2.size());
+}
+
